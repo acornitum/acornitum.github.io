664d4c5c381c58dfb92a969967fb0341f21b37fa
acon <101356372+acornitum@users.noreply.github.com>
2024-06-04 18:40:07 -0400

    it is finally making sense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,44 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main>
+      <div className="flex h-screen px-[10vw] py-0">
+        <div className="w-[50vw] flex flex-col justify-center">
+
+          <div className="text-[30px]">
+            <p>hi! i'm acon :D </p>
+            
+          </div>
+          
+
+          <div className="mt-4">
+            <p>welcome to my corner of the webs!</p>
+            <p>I'm a high school student from Waterloo, Ontario.</p>
+
+            <div className="mt-4">
+              <p>Some things about me:</p>
+            </div>
+
+            
+          </div>
+
+          <div className="mt-4 ml-4">
+            <p>-> I code! Or, try to, at least. Check out my (probably empty) GitHub here!</p>
+            <p>-> Sometimes I dabble in art</p>
+            <p>-> I like yapping - wait, sorry, I mean debating (BP best format)</p>
+            <p>-> I'm a trading card game enthusiast - I play Magic the Gathering!</p>
+            <p>-> This spring, I organized Apocalypse, a 44h hackathon for 150 high schoolers!</p>
+          </div>
+        </div>
+
+        <div className="">
 
 
-        <p>
-        hi! i'm acon :D {"\n"}
-        welcome to my corner of the webs!
-        </p>
 
-        <p>I'm a high school student from Waterloo, Ontario.</p>
+        </div>
+
+
+        
 
 
       </div>
