8f33c1d71a1a8029e06f1f3f11710671eafe41b3
acon <101356372+acornitum@users.noreply.github.com>
2024-06-13 14:20:28 -0400

    hoping to not break anything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,17 +38,37 @@ export default function Home() {
 
         </div>
 
-        <div className="">
-
-
-
+        <div className="landing-right">
+          
         </div>
 
+      </div>
 
-        
 
+
+
+
+      <div className="blog bg-[#232323] flex items-center flex-wrap flex-row justify-center pt-20 pb-5 px-[10vw] py-0;">
+
+        <div className="post h-[400px] w-[300px] border-[color:white] border-[length:2px] rounded-[5px]"><a href="./blog/hackathon-ux-design-philosophy"> 
+
+          <div className="post-image h-[200px] w-full bg-[#333333] rounded-t-[5px]"></div>
+          <div className="post-content p-4">
+            <p className="text-[rgb(245,126,126)] pb-2">my hackathon ux design philosophy</p>
+            <p className="text-[color:white] text-xs pb-6">or, ramblings on why i bother with designing hacker interactivity</p>
+            <p className="text-[color:white] text-xs">date: 2024/06/12</p>
+          </div>
+
+        </a></div>
 
       </div>
+
+      <div className="bg-[#232323] flex justify-center text-xs pb-5 pt-20">
+        <p><a href="https://github.com/acornitum" className="text-[rgb(245,126,126)]">made by acon © 2024</a></p>
+      </div>
+
+
+
     </main>
   );
 }
